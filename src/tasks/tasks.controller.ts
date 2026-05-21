97b6c156e7833626a405d86340fd67a300e34310
Reina <reinasosa2024@gmail.com>
2026-05-21 08:33:46 -0600

    categoria dentro de task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,12 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Put,
-  Delete,
-  Param,
-  Body,
-  ParseIntPipe,
+       Controller,
+       Get,
+       Post,
+       Put,
+       Delete,
+       Param,
+       Body,
+       ParseIntPipe,
 } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
@@ -14,47 +14,48 @@ import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private readonly tasksService: TasksService) {}
+       constructor(private readonly tasksService: TasksService) { }
 
-  // Crear tarea
-  @Post()
-  createTask(
-    @Body() body: { titulo: string; userID: number },
-  ): Promise<Task> {
-    return this.tasksService.createTask(
-      body.titulo,
-      body.userID,
-    );
-  }
+       // Crear tarea
+       @Post()
+       createTask(
+              @Body() body: { titulo: string; userID: number; categoriaId: number },
+       ) {
+              return this.tasksService.createTask(
+                     body.titulo,
+                     body.userID,
+                     body.categoriaId,
+              );
+       }
 
-  // Obtener todas
-  @Get()
-  findAll(): Promise<Task[]> {
-    return this.tasksService.findAll();
-  }
+       // Obtener todas
+       @Get()
+       findAll(): Promise<Task[]> {
+              return this.tasksService.findAll();
+       }
 
-  // Obtener por ID
-  @Get(':id')
-  findById(
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<Task> {
-    return this.tasksService.findById(id);
-  }
+       // Obtener por ID
+       @Get(':id')
+       findById(
+              @Param('id', ParseIntPipe) id: number,
+       ): Promise<Task> {
+              return this.tasksService.findById(id);
+       }
 
-  // Actualizar
-  @Put(':id')
-  updateTask(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() data: Partial<Task>,
-  ): Promise<Task> {
-    return this.tasksService.updateTask(id, data);
-  }
+       // Actualizar
+       @Put(':id')
+       updateTask(
+              @Param('id', ParseIntPipe) id: number,
+              @Body() data: Partial<Task>,
+       ): Promise<Task> {
+              return this.tasksService.updateTask(id, data);
+       }
 
-  // Eliminar
-  @Delete(':id')
-  deleteTask(
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<void> {
-    return this.tasksService.deleteTask(id);
-  }
+       // Eliminar
+       @Delete(':id')
+       deleteTask(
+              @Param('id', ParseIntPipe) id: number,
+       ): Promise<void> {
+              return this.tasksService.deleteTask(id);
+       }
 }
